e03f3a29f4a2ed7657e7a6833855d4e1397b3cab
gyovannyvpn123 <mdanut159@gmail.com>
2025-07-07 10:08:32 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,17 @@
+// index.js
+
+import { InstagramClient } from './lib/client.js';
+import * as Direct from './lib/direct.js';
+import * as Posts from './lib/posts.js';
+import * as User from './lib/user.js';
+import * as Utils from './lib/utils.js';
+import * as GetUserInfo from './lib/getuserinfo.js';
+
+export {
+  InstagramClient,
+  Direct,
+  Posts,
+  User,
+  Utils,
+  GetUserInfo,
+};
